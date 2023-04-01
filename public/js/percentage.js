73ac5fde726e10a3037cca9460ce4e98f9cc2c0d
HugoEnguehard <92102835+HugoEnguehard@users.noreply.github.com>
2023-04-01 19:51:44 +0200

    BDD tests for percentage 1.0

--- public/js/percentage.js
+++ public/js/percentage.js
@@ -1,4 +1,24 @@
-module.exports = {
-    // We will put here the functions we need to implement to make the calculs
+function percentage(value, percentage) {
+    if(isNaN(value) || isNaN(percentage) || typeof(value) !== 'number' || typeof(percentage) !== 'number') return NaN
 
+    if(percentage === 0) return 0
+
+    let result = (value * percentage) / 100
+
+    // For infinite result
+    if(!isFinite(result)) {
+        if(result > 0) return Infinity
+        else return -Infinity
+    }
+
+    // If result is not NaN we check the number of decimals and if superior to 3 then we fix it to 3
+    if(isNaN(result)) return NaN;
+    else { 
+        const decimalCount = (result.toString().split('.')[1] || "").length
+        if(decimalCount > 3) result = parseFloat(result.toFixed(3))
+    }
+
+    return result
 }
+
+module.exports = percentage;
