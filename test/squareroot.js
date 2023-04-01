6656cd015e7d4bc645a4dc3804bd9b784a3ef1cb
HugoEnguehard <92102835+HugoEnguehard@users.noreply.github.com>
2023-04-01 20:14:39 +0200

    BDD tests for squareroot 1.0

--- test/squareroot.js
+++ test/squareroot.js
@@ -1,11 +1,27 @@
 const assert = require('chai').assert;
 const { expect } = require('chai');
-const substraction = require('../public/js/substraction')
+const squareRoot = require('../public/js/squareroot')
 
-// Additions tests
-describe('calculator.js', () => {
-    // it('', () => {
-    //     const result = calculator
-    //     assert
-    // })
+// Square root tests
+describe('SquareRoot', () => {
+    it('SquareRoot of positive number', () => {
+        expect(squareRoot(16)).to.equal(4)
+        expect(squareRoot(16)).to.not.equal(58)
+    })
+    it('SquareRoot of decimal number', () => {
+        expect(squareRoot(12.58)).to.equal(3.547)
+        expect(squareRoot(12.58)).to.not.equal(3.54)
+    })
+    it('SquareRoot of negative number', () => {
+        expect(squareRoot(-16)).to.be.NaN
+        expect(squareRoot(-16)).to.not.equal(4)
+    })
+    it('SquareRoot of zero', () => {
+        expect(squareRoot(0)).to.equal(0)
+        expect(squareRoot(0)).to.not.equal(4)
+    })
+    it('SquareRoot of NaN', () => {
+        expect(squareRoot("64")).to.be.NaN
+        expect(squareRoot("64")).to.not.equal(8)
+    })
 })
