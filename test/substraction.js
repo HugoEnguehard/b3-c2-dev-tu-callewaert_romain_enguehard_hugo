7f826b6a4ba81b403c53a77937b88a02b81092bb
romain callewaert <84373640+zuwui@users.noreply.github.com>
2023-04-02 22:25:11 +0200

    HotFix for test result

--- test/substraction.js
+++ test/substraction.js
@@ -9,35 +9,35 @@ describe('substraction.js', () => {
     //     assert
     // })
     it('Substract two numbers works', () => {
-        const result = substraction.substractNumbers(5, 2)
+        const result = substraction(5, 2)
         assert.equal(result, 3)
     })
     it('Substract two numbers doesn\'t work', () => {
-        const result = substraction.substractNumbers(12, 2)
+        const result = substraction(12, 2)
         assert.notEqual(result, 2)
     })
     it('The result is a number', () => {
-        const result = substraction.substractNumbers(15, 2)
+        const result = substraction(15, 2)
         assert.isNumber(result)
     })
     it('Substract with a string works', () => {
-        const result = substraction.substractNumbers('10', 5)
+        const result = substraction('10', 5)
         assert.equal(result, 5)
     })
     it('Substract with a string works', () => {
-        const result = substraction.substractNumbers('10', '3')
+        const result = substraction('10', '3')
         assert.equal(result, 7)
     })
     it('Substract with a negative number works', () => {
-        const result = substraction.substractNumbers(10, -3)
+        const result = substraction(10, -3)
         assert.equal(result, 13)
     })
     it('Substract with a negative string works', () => {
-        const result = substraction.substractNumbers('-12', 5)
+        const result = substraction('-12', 5)
         assert.equal(result, -17)
     })
     it('Substract 2 great numbers works', () => {
-        const result = substraction.substractNumbers(150954, 14768)
+        const result = substraction(150954, 14768)
         assert.equal(result, 136186)
     })
 })
